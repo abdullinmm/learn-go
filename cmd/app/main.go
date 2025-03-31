0bbfa7b831e1ebf7014d4244ca698ef429148447
Marsel Abdullin <abdullin.marsel@otr.ru>
2025-04-01 00:20:13 +0300

    Add file main.go

---
+++ cmd/app/main.go
@@ -0,0 +1,14 @@
+package main // Обязательная первая строка
+
+import "fmt" // Импорт пакета для вывода
+
+func main() {
+
+	var s1, s2 string
+
+	s1 = "Привет, я изучаю Go!"
+	s2 = "Мой предыдущий язык: MUMPS"
+
+	fmt.Println(s1 + s2)
+
+}
