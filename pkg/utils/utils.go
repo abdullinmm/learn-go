942bc86bbd9aa24b59f4cfa90ff1f8a1019c6e19
Marsel Abdullin <abdullin.marsel@otr.ru>
2025-04-02 13:21:52 +0300

    modified:   ../../pkg/utils/utils.go adde comment

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,6 +2,10 @@ package utils
 
 import "fmt"
 
+/*
+принимает имя (строка) и возвращает приветствие в формате:
+"Привет, {Имя}! Добро пожаловать в Go."
+*/
 func Greet(name string) string {
 	return fmt.Sprintf("Привет, %s! Добро пожаловать в Go.", name)
 }
